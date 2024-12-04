573f7828d5fb94dd658058a54a25b049d99fe42f
ihebjemmali <ihebjemmali000@gmail.com>
2024-12-04 16:25:30 +0100

    update

--- src/Board.js
+++ src/Board.js
@@ -88,6 +88,7 @@ class Board extends EventEmitter {
     }
     return result;
   }
+
   /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
@@ -188,6 +189,7 @@ class Board extends EventEmitter {
     }
     return true;
   }
+
   allColsValid() {
     //check if all the columns in the board are valid
     for (let i = 0; i < this.board.length; i++) {
@@ -197,6 +199,7 @@ class Board extends EventEmitter {
     }
     return true;
   }
+
   allBoxesValid() {
     //check if all the boxes in the board are valid
     for (let i = 0; i < this.board.length; i++) {
@@ -223,19 +226,56 @@ class Board extends EventEmitter {
 =                 TODO: fill in these Solver Functions                    =
 =========================================================================*/
 
+  findEmptyCell() {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (this.board[row][col] === "" || this.board[row][col] === 0) {
+          return [row, col];
+        }
+      }
+    }
+    return null;
+  }
+
   solve() {
     //solve the board using a backtracking algorithm and return the solution
+    let emptyCell = this.findEmptyCell();
+
+    if (!emptyCell) {
+      return true;
+    }
+
+    const [row, col] = emptyCell;
+
+    for (let num = 1; num <= 9; num++) {
+      if (this.isSafe(row, col, num)) {
+        this.board[row][col] = num;
+
+        if (this.solve()) {
+          return true;
+        }
+
+        this.board[row][col] = "";
+      }
+    }
+
+    return false;
   }
 
   solveBoard() {
-    while (this.validBoard()) {
-      if (this.solve()) {
-        this.emit("validBoard", this.board);
-        return true;
-      }
+    if (!this.validBoard()) {
+      this.emit("invalidBoard");
       return false;
     }
+
+    if (this.solve()) {
+      this.emit("validBoard", this.board);
+      return true;
+    }
+
     this.emit("invalidBoard");
-    // dont forget to add a small change here ;)
+    return false;
   }
 }
+
+module.exports = Board;
